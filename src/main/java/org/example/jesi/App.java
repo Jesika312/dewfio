3467782e081317915bb6962112848f56c6ea8275
Jesika312 <118464935+Jesika312@users.noreply.github.com>
2024-05-05 21:49:07 +0700

    Add files via upload

---
+++ src/main/java/org/example/jesi/App.java
@@ -0,0 +1,12 @@
+package org.example.jesi;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App{
+    public static void main(final String[] args){GUI.main(args);}
+}
---
+++ src/main/java/org/example/jesi/HomeControl.java
@@ -0,0 +1,4 @@
+package org.example.jesi;
+
+public class HomeControl {
+}
